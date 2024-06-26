fa386649f7d4240c1e6de53269861cccaa5f1dce
ajmonte0920 <ajmonte0920@gmail.com>
2024-06-26 15:29:04 -0500

    09_da_jun2024_hw_9_sql-challenge

---
+++ EmployeeSQL/ajm_module 9 hmwk_employees sql_analysis.sql
@@ -0,0 +1,85 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/zdZFlQ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(8)   NOT NULL,
+    "dept_name" VARCHAR(60)   NOT NULL,
+    "last_updated" TIMESTAMP DEFAULT Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(8)   NOT NULL,
+    "last_updated" TIMESTAMP DEFAULT Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "id" SERIAL   NOT NULL,
+    "dept_no" VARCHAR(8)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "last_updated" TIMESTAMP DEFAULT Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title" VARCHAR(8)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(150)   NOT NULL,
+    "sex" VARCHAR(8)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    "last_updated" TIMESTAMP DEFAULT Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "salary" FLOAT   NOT NULL,
+    "last_updated" TIMESTAMP DEFAULT Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(8)   NOT NULL,
+    "title" VARCHAR(60)   NOT NULL,
+    "last_updated" TIMESTAMP DEFAULT Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
